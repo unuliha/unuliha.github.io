4dba305ed52710f24e2f0570a0f3e77fcdb1fb35
unuliha <53601088+unuliha@users.noreply.github.com>
2021-08-04 15:12:38 +0800

    Add files via upload

--- scripts/global.js
+++ scripts/global.js
@@ -82,7 +82,7 @@ function showPic(whichpic){
     var placeholder = document.getElementById("placeholder");
     if (source.split(".")[1] == 'jpg'){
         placeholder1.style.display ="none";
-        placeholder.style.display = "block"
+        placeholder.style.display = "block";
         placeholder.setAttribute("src",source);
     } else {
         placeholder.style.display = "none";
@@ -93,7 +93,9 @@ function showPic(whichpic){
 }
 
 function preparePlaceholder(){
-    var gallery = document.getElementById("imagegallery");
+    var gallery = document.getElementById("picSwichArrow");
+    var bigImage = document.createElement('div');
+    bigImage.setAttribute("id","bigImage");
     var placeholder = document.createElement('img');
     placeholder.setAttribute("id","placeholder");
     placeholder.setAttribute("src","images/chuyou.jpg");
@@ -102,8 +104,11 @@ function preparePlaceholder(){
     placeholder1.setAttribute("id","placeholder1");
     placeholder1.setAttribute("controls","all");
     placeholder1.setAttribute("src","images/bodou.mp4");
-    insertAfter(placeholder,gallery);
-    insertAfter(placeholder1,placeholder);
+    bigImage.append(placeholder);
+    bigImage.append(placeholder1);
+    insertAfter(bigImage,gallery);
+    // insertAfter(placeholder,gallery);
+    // insertAfter(placeholder1,placeholder);
     placeholder1.style.display = "none";
 }
 
@@ -224,6 +229,198 @@ function submitFormWithAjax(whichform,thetarget){
     }
 
 
+function swichPic() {
+    var winWidth = document.body.clientWidth;
+    if (winWidth <= 1230) {
+        document.body.className = "grid-1010";
+    } else if (winWidth <= 1410) {
+        document.body.className = "grid-1230";
+    } else if (winWidth > 1410) {
+        document.body.className = "grid-1410";
+    } else {
+        alert("do not know!");
+    }
+
+    //task growth
+    var switchPic = (function () {
+        /*
+    now:当前第几个li
+    linum:总共几个li
+    shownum:要展示几个li
+    w_li:li的宽度
+    marginR_li:li的右边距
+    */
+        var now = 1;
+        var linum, shownum, offset, w_li, marginR_li, pre, next, wrap;
+
+        function init(o) {
+            pre = o.preBtn;
+            next = o.nextBtn;
+            wrap = o.wrap;
+            bindBtn();
+        }
+
+        function btnShow() {
+            getInfo();
+            if (linum <= shownum) { //如果li总个数小于要展示的个数，pre和next都不显示
+                pre.hide();
+                next.hide();
+            } else if (now == 1) { //初始化，只显示next
+                pre.hide();
+                next.show();
+            } else if (now > linum - shownum + 1) { //到最后一组，只显示pre
+                pre.show();
+                next.hide();
+            } else { //中间pre,next都显示。
+                pre.show();
+                next.show();
+            }
+        }
+
+        function getInfo() {
+            linum = $("#imagegallery").find("li").size();
+            shownum = 6;
+            // let body = $("#body");
+            // if (body.hasClass("grid-1010")) {
+            //     shownum = 3;
+            //     w_li = wrap.find("li").outerWidth(); //算上了border的宽度
+            //     marginR_li = parseInt(wrap.find("li").css("marginRight"));
+            //     offset = w_li + marginR_li;
+            // } else if (body.hasClass("grid-1230")) {
+            //     shownum = 4;
+            //     w_li = wrap.find("li").outerWidth(); //算上了border的宽度
+            //     marginR_li = parseInt(wrap.find("li").css("marginRight"));
+            //     offset = w_li + marginR_li;
+            // } else if (body.hasClass("grid-1410")) {
+            //     shownum = 4;
+            //     w_li = wrap.find("li").outerWidth(); //算上了border的宽度
+            //     marginR_li = parseInt(wrap.find("li").css("marginRight"));
+            //     offset = w_li + marginR_li;
+            // }
+        }
+
+        function bindBtn() {
+            btnShow();
+            next.on("click", function () {
+                now += 5;
+                btnShow();
+                // wrap.stop(true).animate({"margin-left": -(now - 1) * offset});
+                wrap.stop(true).animate({"margin-left": -(now - 1) * 100});
+            });
+            pre.on("click", function () {
+                now -= 5;
+                btnShow();
+                wrap.stop(true).animate({"margin-left": -(now - 1) * 100});
+            });
+
+            $(window).resize(function () {
+                now = 1;
+                btnShow();
+                wrap.animate({"margin-left": 0});
+            });
+        }
+
+        return {init: init}
+    })();
+
+    switchPic.init({
+        preBtn: $(".arrowbtn_left"),
+        nextBtn: $(".arrowbtn_right"),
+        wrap: $("#imagegallery")
+    })
+}
+
+function bigSwich() {
+    //添加箭头
+    let $left_arrow = $("<div id='big_left'>" +
+        "<img src='images/left_arrow.png' style='width: 100%'/>" +
+        "</div>");
+    let $right_arrow = $("<div id='big_right'>" +
+        "<img src='images/right_arrow.png' style = 'width:100%'/>" +
+        "</div>")
+    $("#bigImage").append($left_arrow);
+    $("#bigImage").append($right_arrow);
+
+    //将图片保存在数组中
+    let arr = new Array();
+    let linum = $("#imagegallery").find("li").size();
+    for (let i = 0; i < linum; i++) {
+        arr.push($(`#imagegallery li:eq(${i}) a`).attr("href"));
+    }
+
+    //创建对象封装功能
+    let bigSwichVa = (function () {
+        let current_img_index, place,pre,next;
+
+        function init(o) {
+            pre = o.preBtn;
+            next = o.nextBtn;
+            bindBtn();
+        }
+
+        function bigBtnShow() {
+             if (current_img_index < 1) {
+                pre.hide();
+                next.show();
+            } else if (current_img_index > arr.length - 2) {
+                pre.show();
+                next.hide();
+            } else {
+                pre.show();
+                next.show();
+            }
+        }
+
+        function getplace(index) {
+            if (arr[index].split(".")[1] == 'jpg') {
+                $("#placeholder").css("display" , "block");
+                $("#placeholder1").css("display" , "none");
+                place = $("#placeholder");
+            } else {
+                $("#placeholder").css("display" , "none");
+                $("#placeholder1").css("display" , "block");
+                place = $("#placeholder1");
+            }
+            return place;
+        }
+
+        function getIndex(){
+            let nowplace = ($("#placeholder").is(":hidden"))?
+                ($("#placeholder1")):($("#placeholder"));
+            let nowPic = nowplace.attr("src");
+            current_img_index = arr.indexOf(nowPic);
+        }
+        function bindBtn() {
+            window.onclick = function (){
+                getIndex();
+                bigBtnShow();
+            }
+            next.on("click", function () {
+                getIndex();
+                current_img_index++;
+                getplace(current_img_index);
+                place.attr("src",arr[current_img_index]);
+                bigBtnShow();
+            })
+            pre.on("click", function () {
+                getIndex();
+                current_img_index--;
+                bigBtnShow();
+                place = getplace(current_img_index);
+                place.attr("src",arr[current_img_index]);
+                bigBtnShow();
+            });
+        }
+
+        return {init: init}
+    })();
+    bigSwichVa.init({
+            preBtn: $("#big_left img"),
+            nextBtn: $("#big_right img"),
+        })
+    }
+
+
 addLoadEvent(prepareForms);
 addLoadEvent(prepareInternalnav);
 addLoadEvent(highlightPage);
@@ -231,4 +428,5 @@ addLoadEvent(stripeTables);
 addLoadEvent(highlightRows);
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
-
+addLoadEvent(swichPic);
+addLoadEvent(bigSwich);
